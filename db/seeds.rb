03bdcdc1184656d67afb38b786be104cb323cb58
Lidia Setubal <lidiasetubalbarbosa@hotmail.com>
2022-05-25 16:39:33 -0300

    restaurant rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+dishoom = Restaurant.create(name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "+8747628119", category: "chinese")
+paris = Restaurant.create(name: "paris 6", address: "8 São paulo", phone_number: "+8747628119", category: "french")
+canarinho = Restaurant.create(name: "Canarinho", address: "Pablo picasso 100", phone_number: "+879999119", category: "italian")
+jesuspizza = Restaurant.create(name: "Jesus Pizza", address: "Av Uruguai 1199", phone_number: "+8746666119", category: "chinese")
+pizza_east = Restaurant.create(name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "+8000119", category: "melgian")
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
